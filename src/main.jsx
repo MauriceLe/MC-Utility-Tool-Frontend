5fe1c7fcd9654e565e5d77da93aef27af4efefca
Leenders <Leonmaurice.Leenders@oetker.com>
2023-11-28 15:03:40 +0100

    Changed react router for routing error github pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider/>
   </React.StrictMode>,
 )
