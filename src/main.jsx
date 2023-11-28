ae4557280597138909d535f3cbea0dec3e152ab3
Leenders <Leonmaurice.Leenders@oetker.com>
2023-11-28 14:38:19 +0100

    Changed react router variant for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
